831dee5bfba648b9530d70fb734056f7b261f562
prashant kharat <prashant00095@gmail.com>
2025-02-04 19:28:53 +0530

    remove the predefined values

--- Models/Lr.cs
+++ Models/Lr.cs
@@ -12,98 +12,97 @@ namespace MauiAppFB.Models
 
 
 
-
-        public int? LrCompanyId { get; set; } = 1;
-            public int? LrAddedByUserId { get; set; } = 1;
-        public int? LrVoucherId { get; set; } = 1;
-        public int? LrPartyAccountId { get; set; } = 1;
-        public int?  LrConsigneeNotifyId { get; set; } = 1;
-        public int? LrConsignorNotifyId { get; set; } = 1;
-        public int? LrProductId { get; set; } = 1;
-        public int? LrVehicleId { get; set; } = 1;
-        public int? LrSupplierAccountId { get; set; } = 1;
-        public int? LrDriverId { get; set; } = 1;
-        public int? LrNo { get; set; } = 1;
-        public string? LrNostr { get; set; } = "1";
+ public int? LrCompanyId { get; set; }
+         public int? LrAddedByUserId { get; set; }  
+        public int? LrVoucherId { get; set; }  
+        public int? LrPartyAccountId { get; set; }  
+        public int?  LrConsigneeNotifyId { get; set; }  
+        public int? LrConsignorNotifyId { get; set; }  
+        public int? LrProductId { get; set; }  
+        public int? LrVehicleId { get; set; }  
+        public int? LrSupplierAccountId { get; set; }  
+        public int? LrDriverId { get; set; }  
+        public int? LrNo { get; set; }  
+        public string? LrNostr { get; set; }  
         public DateTime? LrDate { get; set; } = DateTime.UtcNow.Date;
-        public string? LrTime { get; set; } = "12";
-        public int? LrTripno { get; set; } = 1;
-            public int? LrFromLocationId { get; set; } = 1;
-        public int? LrToLocationId { get; set; } = 1;
-        public int? LrBackLocationId { get; set; } = 1;
-        public string? LrContainer1 { get; set; } = "1";
-        public string? LrContainer2 { get; set; } = "1";
-            public double? LrGrosswt { get; set; } = 1.5;
-        public double? LrTarewt { get; set; } = 1.5;
+        public string? LrTime { get; set; } 
+        public int? LrTripno { get; set; }  
+            public int? LrFromLocationId { get; set; }  
+        public int? LrToLocationId { get; set; }  
+        public int? LrBackLocationId { get; set; }  
+        public string? LrContainer1 { get; set; }  
+        public string? LrContainer2 { get; set; }  
+            public double? LrGrosswt { get; set; } 
+        public double? LrTarewt { get; set; } 
         public double? LrLoadwt { get; set; } = 55;
-        public double? LrChargeqty { get; set; } = 1.5;
-        public double? LrUnloadwt { get; set; } = 1.5;
-        public double? LrShortwt { get; set; } = 1.5;
-        public double? LrShortallowBill { get; set; } = 1.5;
-        public double? LrShortPerBill { get; set; } = 1.5;
-        public double? LrShortPerTruck { get; set; } = 1.5;
-        public string? LrShortallowtype { get; set; } = "1";
-            public double? LrShortnetBill { get; set; } = 1.5;
-        public double? LrShortratebill { get; set; } = 1.5;
-        public double? LrShortratetruck { get; set; } = 1.5;
-        public double? LrShortamtbill { get; set; } = 1.5;
-        public double? LrShortamttruck { get; set; } = 1.5;
-        public string? LrPaymenttype { get; set; } = "1";
-        public double? LrRatebill { get; set; } = 1.5;
-        public double? LrBilltypebill { get; set; } = 1.5;
-        public double? LrGrossfreightbill { get; set; } = 1.5;
-        public double? LrTripchargebill { get; set; } = 1.5;
-        public double? LrAdvancebill { get; set; } = 1.5;
-        public double LrNetfreightbill { get; set; } = 1.5;
-        public string? LrRefby { get; set; } = "1";
-        public string? LrStartkm { get; set; } = "1";
-        public string? LrEndkm { get; set; } = "1";
-            public double? LrBilltypetruck { get; set; } = 1.5;
-        public double? LrBillratetruck { get; set; } = 1.5;
-        public double? LrGrossfreighttruck { get; set; } = 1.5;
-        public double? LrTripchargetruck { get; set; } = 1.5;
-        public double? LrTripadvance { get; set; } = 1.5;
-        public double? LrCashbank { get; set; } = 1.5;
-        public double? LrNetfreighttruck { get; set; } = 1.5;
-        public string? LrCustom1 { get; set; } = "1";
-            public string? LrCustom2 { get; set; } = "1";
-        public string? LrCustom3 { get; set; } = "1";
-        public string? LrCustom4 { get; set; } = "1";
-        public string? LrInvoiceno { get; set; } = "1";
-        public double? LrInvoicevalue { get; set; } = 1.5;
-            public string? LrInvoicedate { get; set; } = "1";
-        public string? LrDono { get; set; } = "1";
-        public string? LrDodate { get; set; } = "1";
-        public string? LrEwaybillno { get; set; } = "1";
-        public string? LrSbno { get; set; } = "1";
-        public string? LrRemarks { get; set; } = "1";
-        public string? LrReportDate { get; set; } = "1";
-        public string? LrReportTime { get; set; } = "1";
-        public string? LrUnloadDate { get; set; } = "1";
-        public string? LrUnloadTime { get; set; } = "1";
-        public string? LrPodDate { get; set; } = "1";
-        public string? LrReturnDate { get; set; } = "1";
-        public double? LrDetentionRatebill { get; set; } = 1.5;
-        public double? LrDetentionRatetruck { get; set; } = 1.5;
-        public double? LrDetentionDaybill { get; set; } = 1.5;
-        public double? LrDetentionDaytruck { get; set; } = 1.5;
-        public double? LrDetentionAmtbill { get; set; } = 1.5;
-        public double? LrDetentionAmttruck { get; set; } = 1.5;
-        public string? LrApprovedBy { get; set; } = "1";
-        public int? LrChaId { get; set; } = 1;
-        public int? LrVesselId { get; set; } = 1;
-            public string? LrGatepassno { get; set; } = "1";
-        public int? LrPartyBillId { get; set; } = 1;
-            public int? LrSupplierBillId { get; set; } = 1;
-        public int? LrAllowancePayId { get; set; } = 1;
-        public int? LrAdvancePayId { get; set; } = 1;
-        public int? LrAdvanceRecId { get; set; } = 1;
-        public double? LrTotkm { get; set; } = 1.5;
-        public double? LrTotalbags { get; set; } = 1.5;
-        public string? LrTriptype { get; set; } = "1";
-        public string? LrGroupno { get; set; } = "1";
-            public double? LrAllowAmt { get; set; } = 1.5;
-        public double? LrAllowDieselQty { get; set; } = 1.5;
+        public double? LrChargeqty { get; set; } 
+        public double? LrUnloadwt { get; set; } 
+        public double? LrShortwt { get; set; } 
+        public double? LrShortallowBill { get; set; } 
+        public double? LrShortPerBill { get; set; } 
+        public double? LrShortPerTruck { get; set; } 
+        public string? LrShortallowtype { get; set; }  
+            public double? LrShortnetBill { get; set; } 
+        public double? LrShortratebill { get; set; } 
+        public double? LrShortratetruck { get; set; } 
+        public double? LrShortamtbill { get; set; } 
+        public double? LrShortamttruck { get; set; } 
+        public string? LrPaymenttype { get; set; }  
+        public double? LrRatebill { get; set; } 
+        public double? LrBilltypebill { get; set; } 
+        public double? LrGrossfreightbill { get; set; } 
+        public double? LrTripchargebill { get; set; } 
+        public double? LrAdvancebill { get; set; } 
+        public double LrNetfreightbill { get; set; } 
+        public string? LrRefby { get; set; }  
+        public string? LrStartkm { get; set; }  
+        public string? LrEndkm { get; set; }  
+            public double? LrBilltypetruck { get; set; } 
+        public double? LrBillratetruck { get; set; } 
+        public double? LrGrossfreighttruck { get; set; } 
+        public double? LrTripchargetruck { get; set; } 
+        public double? LrTripadvance { get; set; } 
+        public double? LrCashbank { get; set; } 
+        public double? LrNetfreighttruck { get; set; } 
+        public string? LrCustom1 { get; set; }  
+            public string? LrCustom2 { get; set; }  
+        public string? LrCustom3 { get; set; }  
+        public string? LrCustom4 { get; set; }  
+        public string? LrInvoiceno { get; set; }  
+        public double? LrInvoicevalue { get; set; } 
+            public string? LrInvoicedate { get; set; }  
+        public string? LrDono { get; set; }  
+        public string? LrDodate { get; set; }  
+        public string? LrEwaybillno { get; set; }  
+        public string? LrSbno { get; set; }  
+        public string? LrRemarks { get; set; }  
+        public string? LrReportDate { get; set; }  
+        public string? LrReportTime { get; set; }  
+        public string? LrUnloadDate { get; set; }  
+        public string? LrUnloadTime { get; set; }  
+        public string? LrPodDate { get; set; }  
+        public string? LrReturnDate { get; set; }  
+        public double? LrDetentionRatebill { get; set; } 
+        public double? LrDetentionRatetruck { get; set; } 
+        public double? LrDetentionDaybill { get; set; } 
+        public double? LrDetentionDaytruck { get; set; } 
+        public double? LrDetentionAmtbill { get; set; } 
+        public double? LrDetentionAmttruck { get; set; } 
+        public string? LrApprovedBy { get; set; }  
+        public int? LrChaId { get; set; }  
+        public int? LrVesselId { get; set; }  
+            public string? LrGatepassno { get; set; }  
+        public int? LrPartyBillId { get; set; }  
+            public int? LrSupplierBillId { get; set; }  
+        public int? LrAllowancePayId { get; set; }  
+        public int? LrAdvancePayId { get; set; }  
+        public int? LrAdvanceRecId { get; set; }  
+        public double? LrTotkm { get; set; } 
+        public double? LrTotalbags { get; set; } 
+        public string? LrTriptype { get; set; }  
+        public string? LrGroupno { get; set; }  
+            public double? LrAllowAmt { get; set; } 
+        public double? LrAllowDieselQty { get; set; } 
 
 
 
