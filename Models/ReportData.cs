117036413e6153917771d5e8afcd10eec8f37d46
prashant kharat <prashant00095@gmail.com>
2025-05-06 11:35:14 +0530

    LR report  changes

---
+++ Models/ReportData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppFB.Models
+{
+    public class ReportData
+    {
+        public int ReportDataId { get; set; }
+        public string ReportDataDocType { get; set; }
+        public string ReportDataLayoutData { get; set; }
+        public string ReportDataFormatName { get; set; }
+        public int ReportDataCompanyId { get; set; }
+        public int ReportDataStatus { get; set; }
+    }
+
+}
